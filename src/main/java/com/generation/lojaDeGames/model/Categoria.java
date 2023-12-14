00bcade8b3ddd8b1091fc564cc64dd4470ffe482
wesley berto <wesleyberto4@gmail.com>
2023-12-14 15:32:50 -0300

    model, repository e controller de produto

--- src/main/java/com/generation/lojaDeGames/model/Categoria.java
+++ src/main/java/com/generation/lojaDeGames/model/Categoria.java
@@ -1,10 +1,17 @@
 package com.generation.lojaDeGames.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -33,8 +40,10 @@ public class Categoria {
 	@Column(length = 100)
 	private String recursos;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 	
-
 	public Long getId() {
 		return id;
 	}
@@ -66,5 +75,14 @@ public class Categoria {
 	public void setRecursos(String recursosEspecificos) {
 		this.recursos = recursosEspecificos;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
 
 }
---
+++ src/main/java/com/generation/lojaDeGames/model/Produto.java
@@ -0,0 +1,82 @@
+package com.generation.lojaDeGames.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+	
+	@NotBlank(message = "O Atributo nome é obrigatorio")
+	private String nome;
+	
+	@NotNull(message = "O preço é obrigatório!")
+	@Positive(message = "O preço deve ser maior que zero!")
+	@Column(name="preco", precision=8, scale=2)
+	private BigDecimal preco;
+	
+	private String descricao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+}
